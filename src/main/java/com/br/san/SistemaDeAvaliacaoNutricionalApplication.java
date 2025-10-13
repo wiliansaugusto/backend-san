d4a7e3959b30b3db37d36f0158fe693c1e9f310f
wiliansaugusto <wiliansaugusto@gmail.com>
2025-10-13 13:55:04 -0300

    Primeiro commit

---
+++ src/main/java/com/br/san/SistemaDeAvaliacaoNutricionalApplication.java
@@ -0,0 +1,13 @@
+package com.br.san;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAvaliacaoNutricionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAvaliacaoNutricionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/san/SistemaDeAvaliacaoNutricionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.san;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeAvaliacaoNutricionalApplicationTests {
+
+	/*@Test
+	    void contextLoads() {
+	}*/
+
+}
